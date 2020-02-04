b9d26dd647925063e6cd6da89c4db03437129edf
pale619 <giorgio@paginemediche.it>
2020-02-04 18:52:23 +0100

    Aggiunta variabile dominio widget

--- esempi/teleconsulto/pubblico/widget/consultazione-domanda.php
+++ esempi/teleconsulto/pubblico/widget/consultazione-domanda.php
@@ -1,6 +1,7 @@
 <?php
 $token_login_utente = ""; /* OTTINIBILE DALLA REGISTRAZIONE E EFFETTUANDO IL LOGIN */
 $numero_domanda = 0;
+$domain = $_SERVER["SERVER_NAME"];
 
 $link = "https://www.paginemediche.it/remote-teleconsulto?token=" . $token_login_utente . "&numero=" . $numero_domanda . "&domain=" . $domain;
 
