4fed0c14ccd3064bcb0bbd7f453a81b66570296f
pale619 <giorgio@paginemediche.it>
2020-02-05 14:14:51 +0100

    Pubblicazione API lista categorie

--- esempi/teleconsulto/pubblico/api/lista-categorie.php
+++ esempi/teleconsulto/pubblico/api/lista-categorie.php
@@ -9,11 +9,10 @@ $headers = array(
     "domain: " . $domain
     , "client-id: " . $client_id
     , "client-secret: " . $client_secret
-    , "t: " . $token_login_utente
 );
 
 $ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://api.paginemediche.it/api/1.0/tc-get-patient-question-list");
+curl_setopt($ch, CURLOPT_URL, "https://api.paginemediche.it/api/1.0/tc-get-categories");
 curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_VERBOSE, true);
